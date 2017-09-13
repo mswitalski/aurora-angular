97de44adce289b5eaeed8969d6ca584a0adf282c
Marek Świtalski <marek.switalski@outlook.com>
2017-09-13 22:47:19 +0200

    Introduced basic layout (only header) and shared components module.
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './shared';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HeaderComponent
     ],
     imports: [
         BrowserModule
@@ -13,4 +14,6 @@ import { AppComponent } from './app.component';
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+import {HttpModule} from "@angular/http";
+import {RouterModule} from "@angular/router";
+
+@NgModule({
+    imports: [
+        HttpModule,
+        RouterModule
+    ],
+    exports: [
+        HttpModule,
+        RouterModule
+    ]
+})
+export class SharedModule {
+}
