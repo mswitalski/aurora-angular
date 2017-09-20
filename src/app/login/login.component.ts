037efb56a1e88ffa5cec48d7d44fab9a5a744e6f
Marek Świtalski <marek.switalski@outlook.com>
2017-09-20 14:42:26 +0200

    Improved UI. Added font awesome.
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,7 +7,8 @@ import {Subscription} from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-login-page',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnDestroy {
     isSubmitting = false;
