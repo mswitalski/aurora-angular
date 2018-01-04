350d6e71c07b7884dd94143db0f87d529e3f3a72
Marek Świtalski <marek.switalski@outlook.com>
2018-01-04 16:28:42 +0100

    Forgot to add https to redirect_uri for outlook
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     backendUrl: 'https://localhost:8443/api/v1/',
     loginUrl: 'https://localhost:8443/login',
-    outlookLoginUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=1fe59cb2-fd3f-4ecf-a9e4-77dc1eea9004&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2F&scope=openid%20https%3A%2F%2Fgraph.microsoft.com%2Fcalendars.readwrite&response_mode=fragment&state=outlook&nonce=678910',
+    outlookLoginUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=1fe59cb2-fd3f-4ecf-a9e4-77dc1eea9004&response_type=token&redirect_uri=https%3A%2F%2Flocalhost%3A4200%2F&scope=openid%20https%3A%2F%2Fgraph.microsoft.com%2Fcalendars.readwrite&response_mode=fragment&state=outlook&nonce=678910',
     resultsOnPage: 20,
     adminRole: 'ADMIN',
     unitLeaderRole: 'UNIT_LEADER',
