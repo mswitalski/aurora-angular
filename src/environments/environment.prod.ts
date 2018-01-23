8effc57db54916bb1f5c0b461b2b3e1bb74d3910
Marek Świtalski <marek.switalski@outlook.com>
2018-01-23 21:11:31 +0100

    Changed outlook redirect ports
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     loginUrl: 'https://localhost:8443/login',
     outlookLoginUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?'
         + 'client_id=1fe59cb2-fd3f-4ecf-a9e4-77dc1eea9004&response_type=token&'
-        + 'redirect_uri=https%3A%2F%2Flocalhost%3A4200%2F&'
+        + 'redirect_uri=https%3A%2F%2Flocalhost%3A8443%2F&'
         + 'scope=openid%20https%3A%2F%2Fgraph.microsoft.com%2Fcalendars.readwrite'
         + '&response_mode=fragment&state=outlook&nonce=678910', resultsOnPage: 20,
     adminRole: 'ADMIN',
