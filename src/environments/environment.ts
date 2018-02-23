186d14ee67b7b5b87b1072860d09798bec09a289
Marek Świtalski <marek.switalski@outlook.com>
2018-02-23 13:39:37 +0100

    Removed no longer valid client ID for Microsoft Outlook API
    Signed-off-by: Marek Świtalski <marek.switalski@outlook.com>

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
     backendUrl: 'https://localhost:8443/api/v1/',
     loginUrl: 'https://localhost:8443/login',
     outlookLoginUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize?'
-        + 'client_id=1fe59cb2-fd3f-4ecf-a9e4-77dc1eea9004&response_type=token&'
+        + 'client_id=YOUR_ID&response_type=token&'
         + 'redirect_uri=https%3A%2F%2Flocalhost%3A8443%2F&'
         + 'scope=openid%20https%3A%2F%2Fgraph.microsoft.com%2Fcalendars.readwrite'
         + '&response_mode=fragment&state=outlook&nonce=678910',
